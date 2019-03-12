b8e41c25af415c85035e5fc6822ea46a19c7be6d
Oliver Cripps <ollycripps@gmail.com>
2019-03-12 14:55:11 +0000

    add several rounds feature

--- dice-game/dice-game-v3.rb
+++ dice-game/dice-game-v3.rb
@@ -2,11 +2,27 @@
 # Upgrade the dice game: now both I and my opponent roll two six-sided dice each.
 # Upgrade the game once more: now we play several rounds for each game.
 
-player_1 = rand(6) + 1
-player_2 = rand(6) + 1
+player_1_score = 0
+player_2_score = 0
+rounds = 0
+
+while rounds < 3 do
+
+player_1 = rand(1..6) + rand(1..6)
+player_2 = rand(1..6) + rand(1..6)
+rounds += 1
 
 if player_1 > player_2
-  puts "player_1 wins with #{player_1}"
+  puts "Player 1 wins with: #{player_1}"
+  player_1_score += 1
+  puts "Player 1 score: #{player_1_score}\nPlayer 2 score: #{player_2_score}"
+elsif player_2 > player_1
+  puts "Player 2 wins with: #{player_2}"
+  player_2_score += 1
+  puts "Player 1 score: #{player_1_score}\nPlayer 2 score: #{player_2_score}"
 else
-  puts "player_2 wins with #{player_2}"
+  puts "DRAW: Player 1 scored #{player_1} and Player 2 scored #{player_2}"
+  puts "Player 1 score: #{player_1_score}\nPlayer 2 score: #{player_2_score}"
+end
+
 end
