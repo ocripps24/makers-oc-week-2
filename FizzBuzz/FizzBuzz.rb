afbc2b0b63ed1bf73db7211130af21f7a7dd8449
Oliver Cripps <ollycripps@gmail.com>
2019-03-13 12:46:27 +0000

    update to FizzBuzz program using better procedure

--- FizzBuzz/FizzBuzz.rb
+++ FizzBuzz/FizzBuzz.rb
@@ -8,26 +8,6 @@
 #     * If the number is divisible by neither 3 or 5, keep the number.
 # * `puts`es these "FizzBuzz sequence" entries, one per line.
 
-=begin
-num = 1
-
-while num < 21 do
-  if (num % 3 == 0 && num % 5 == 0)
-    puts "FizzBuzz"
-    num += 1
-  elsif num % 5 == 0
-    puts "Buzz"
-    num += 1
-  elsif num % 3 == 0
-    puts "Fizz"
-    num += 1
-  else
-    puts num
-    num += 1
-  end
-end
-=end
-
 num = 0
 
 while num < 20
@@ -43,8 +23,18 @@ while num < 20
   end
 end
 
+# OR YOU CAN DO IT LIKE THIS
+
 =begin
-for num in 1..20 do
-  puts num
+for num in 1..100
+  if num % 15 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0
+    puts "Fizz"
+  else
+    puts num
+  end
 end
 =end
